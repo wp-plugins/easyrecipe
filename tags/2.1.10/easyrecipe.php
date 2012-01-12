4cc0846b2ba852aff857def4004fc24d59bcc373
Jayce53 <Jayce53@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-12 22:24:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easyrecipe@489046 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.1.10/easyrecipe.php
@@ -0,0 +1,54 @@
+<?php
+
+  /*
+    Plugin Name: Easy Recipe
+    Plugin URI: http://www.orgasmicchef.com/easyrecipe/
+    Description: Create, edit, display and print recipes with hRecipe microformat functionality
+    Author: Orgasmic Chef
+    Version: 2.1.10
+    Author URI: http://www.orgasmicchef.com
+   */
+
+  // Make sure we don't expose any info if called directly
+  if (!function_exists('add_action')) {
+    echo "Hi there!  I'm just a plugin, not much I can do when called directly.";
+    exit;
+  }
+
+  function easyrecipeNeedPHP5() {
+    wp_die("Easy Recipe requires PHP 5+.  Your server is running PHP" . phpversion() . '<br /><a href="/wp-admin/plugins.php">Go back</a>');
+  }
+
+  function easyrecipeNeedDOM() {
+    wp_die("Easy Recipe requires the PHP DOMDocument extension but it has been disabled in your server's PHP" . phpversion() . '<br /><a href="/wp-admin/plugins.php">Go back</a>');
+  }
+
+  if (phpversion() < '5') {
+    register_activation_hook(__FILE__, "easyrecipeNeedPHP5");
+    return;
+  }
+
+  if (!class_exists("DOMDocument")) {
+    register_activation_hook(__FILE__, "easyrecipeNeedDOM");
+    return;
+  }
+
+//  register_activation_hook(__FILE__, "easyrecipeActivated");
+  /*
+   * If we're in admin, we only care about specific pages/actions
+   * Don't waste time with stuff where the plugin isn't needed
+   */
+  if (is_admin()) {
+    if ($GLOBALS["pagenow"] == "admin-ajax.php") {
+      if (!isset($_REQUEST["action"]) || ($_REQUEST["action"] != "ERsendDiagnostics" && $_REQUEST["action"] != "customCSS")) {
+        return;
+      }
+    }
+  }
+  if (!class_exists('EasyRecipe')) {
+    require_once 'class-easyrecipe.php';
+    $er = new EasyRecipe();
+    register_activation_hook(__FILE__, array($er, "easyrecipeActivated"));
+    register_deactivation_hook(__FILE__, array($er, "easyrecipeDeactivated"));
+  }
+?>
