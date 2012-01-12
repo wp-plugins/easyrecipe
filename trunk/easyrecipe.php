9ad8f48e14911fca0c8b67b804e0d316f50ed955
Jayce53 <Jayce53@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-12 12:44:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easyrecipe@488706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/easyrecipe.php
+++ trunk/easyrecipe.php
@@ -5,7 +5,7 @@
     Plugin URI: http://www.orgasmicchef.com/easyrecipe/
     Description: Create, edit, display and print recipes with hRecipe microformat functionality
     Author: Orgasmic Chef
-    Version: 2.1.8
+    Version: 2.1.9
     Author URI: http://www.orgasmicchef.com
    */
 
